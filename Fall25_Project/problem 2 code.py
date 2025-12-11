c5ac3e0a35dbf149212d3073f8d87c4a4c5ad234
nicholas santorelli <nicholassantorelli0@gmail.com>
2025-12-10 22:51:44 -0500

    libraries

--- Fall25_Project/problem 2 code.py
+++ Fall25_Project/problem 2 code.py
@@ -1,3 +1,10 @@
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
 #Question 2 Code
 print("\n" + "=" * 60)
 print("QUESTION 2: Can weather predict next day's traffic?")
